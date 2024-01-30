fc1f85ebd0269eead7deefb58170582b9781e0e5
moritoraji <freelance0507@gmail.com>
2024-01-31 00:00:41 +0900

    修正完了

--- src/js/script.js
+++ src/js/script.js
@@ -2,10 +2,13 @@ import { common } from "./_common";
 import { swiper } from "./_swiper";
 import { acodion } from "./_acodion";
 import { smooth } from "./_smooth";
+// import { smooth } from "./_topbtn";
 import { test } from "./_test";
 
 common();
 swiper();
 acodion();
 smooth();
+// topbtn();
 test();
+
